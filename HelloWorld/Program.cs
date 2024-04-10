3082c3d266adf8fa615fb0fd0e7dc3f3c01b3061
Manoel <999142818m@gmail.com>
2024-04-10 13:18:43 -0300

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Hello
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World");
+
+        var texto = "lorem imput text for bla bla bla bla";
+        Console.WriteLine(texto);
+
+        Console.WriteLine("whats your name?");
+        var name = Console.ReadLine();
+        var currentDate = DateTime.Now;
+
+        Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+        Console.Write($"{Environment.NewLine}Press any key to exit...");
+        Console.ReadKey(true);
+    }
+}
