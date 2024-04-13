cf9d30a756d4c5c335ecb306ba9592dea5689e08
Manoel <999142818m@gmail.com>
2024-04-13 13:59:58 -0300

    Variaveis e Operadores

---
+++ HelloWorld/Variable.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace HelloWorld
+{
+    internal class Variable
+    {
+        public int a = 99;
+        public double b= 12.12;
+        public float c = 0.1;
+        public decimal d = .123m;
+        public byte f = 255;
+        public string e = "abc";
+        public char g = 'a';
+        public bool h = true;
+
+        // Operadores
+
+        // 6 + 2 = 8 Adição
+        // 6 / 2 = 3 Divisão
+        // 6 * 2 = 12 Multiplicação
+        // 6 - 2 = 4 Subtração
+        // 6 % 2 = 0.12 Restante
+        // = Atribuição
+        // += Incremento
+        // -= Diminuir
+        // *= Atribuição de multiplicação
+        // /= Atribuição de divisão
+        // ++
+        // --
+        // 6 < 2 Menor que (verdadeiro se x for menor que y)
+        // 6 <= 2 Menor ou igual a
+        // 6 > 2 Maior que (verdadeiro se x for maior que y)
+        // 6 >= 2 Maior que ou igual a
+        // 6 == 2 Igualdade
+        // 6 != 2 Não igual
+        // x & y AND lógico ou bit a bi
+        // x && y AND lógico
+        // x | y OR lógico ou bit a bit.
+        // x || y OR lógico
+        // x ^ y XOR lógico ou bit a bit
+        // => Declaração de lambda
+
+        // is Retornará true se o operando esquerdo avaliado puder ser convertido no tipo especificado no operando à direita(um tipoestático)
+        // as .Retorna o operando esquerdo convertido para o tipo especificado pelo operando à direita(um tipo estático), mas as retorna null em que(T)x lançaria uma exceção
+
+
+    }
+}
